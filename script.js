34c7fc9da12caf5957ed88c3b9e75fce7636a401
RitaIssa <rita.issa@devy.com.au>
2024-03-22 10:21:04 -0500

    play a single round

--- script.js
+++ script.js
@@ -7,6 +7,24 @@
 //Choice[0] gives rock, choice[1] gives paper, and choice[2] gives scissors
 
 function getComputerChoice(){
-    let choice = ["Rock", "Paper", "Scissors"]
+    let choice = ["rock", "paper", "scissors"]
     return choice[(Math.floor(Math.random() * (choice.length)))]
 }
+
+//Play a single round
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection == "rock" && computerSelection == "rock"){
+        console.log("Its a tie!");
+    } else if (playerSelection == "rock" && computerSelection == "paper"){
+        console.log("You Lose! Paper beats Rock");
+    } else if (playerSelection == "rock" && computerSelection == "scissors"){
+        console.log("You Win! Rock beats Scissors");
+    }
+}
+  const player = "ROCK";
+  const playerSelection = player.toLowerCase();
+  const computerSelection = getComputerChoice();
+  console.log(computerSelection);
+  console.log(playerSelection);
+  console.log(playRound(playerSelection, computerSelection));
+  
