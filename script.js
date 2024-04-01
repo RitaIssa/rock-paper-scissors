e6e046fb63436c21a553a620d250376416480b89
RitaIssa <rita.issa@devy.com.au>
2024-04-01 13:42:34 -0500

    fixed and updated

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const scissorBtn = document.querySelector("#scissor");
 const div = document.querySelector(".text");
 const human = document.querySelector(".humanScore");
 const computer = document.querySelector(".computerScore");
+const humanChoice = document.querySelector(".playerChoice");
+const computerChoice = document.querySelector(".computerChoice");
+const winner = document.querySelector(".winner");
+const resetBtn = document.querySelector("#reset");
 
 rockBtn.addEventListener("click", () => {
   playRound("rock", getComputerChoice());
@@ -20,6 +24,8 @@ scissorBtn.addEventListener("click", () => {
     score();
 });
 
+resetBtn.addEventListener("click", resetGame);
+
 //Get the computers choice. 
 //It will randomly return rock, paper, or scissors
 //Math.random gets between 0 and less than 1
@@ -34,9 +40,9 @@ function getComputerChoice(){
 
 //Play a single round
 function playRound(playerSelection, computerSelection){
-    
-    console.log(playerSelection);
-    console.log(computerSelection);
+  
+  humanChoice.textContent = "Players Choice: " + playerSelection;
+  computerChoice.textContent = "Computers Choice:" + computerSelection;
 
     if(playerSelection == "rock" && computerSelection == "rock" || playerSelection == "paper" && computerSelection == "paper" || playerSelection == "scissors" && computerSelection == "scissors"){
         div.textContent = "Its a tie!";
@@ -55,47 +61,56 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
-// function score(){
-//     let humanPlayer = 0;
-//     let computerPlayer = 0;
-//     for(i = 1; i <= 5; i++){
-//         if(div.textContent == "Its a tie!"){
-//             human.textContent = "Your Score" + humanPlayer++;
-//             computer.textContent = "Computer Score" + computerPlayer++;
-//         } else if(div.textContent == "You Win!"){
-//             human.textContent = "Your Score" + humanPlayer++;
-//         } else {
-//             computer.textContent = "Computer Score" + computerPlayer++;
-//         }
-//     }
-// }
-
 let humanPlayer = 0;
 let computerPlayer = 0;
+let round = 0;
+human.textContent = "Player Score: " + humanPlayer;
+computer.textContent = "Computer Score: " + computerPlayer;
 
 function score() {
-  for (let i = 1; i <= 5; i++) {
-    if (div.textContent == "Its a tie!") {
+
+    if (div.textContent.includes("Its a tie!")) {
       humanPlayer++;
       computerPlayer++;
-    } else if (div.textContent == "You Win!") {
+    } else if (div.textContent.includes("You Win!")) {
       humanPlayer++;
     } else {
       computerPlayer++;
     }
 
-    human.textContent = "Your Score: " + humanPlayer;
+    human.textContent = "Player Score: " + humanPlayer;
     computer.textContent = "Computer Score: " + computerPlayer;
-  }
+
+    round++;
+
+    if (round == 5) {
+      if(humanPlayer == computerPlayer){
+        winner.textContent = "It's a tie! Try to win next time!!!";
+      } else if (humanPlayer > computerPlayer) {
+        winner.textContent = "Congratulations! You won!!!";
+      } else {
+        winner.textContent = "Oufff! Better luck next time ;(";
+      }
+      resetBtn.style.display = "block";
+      rockBtn.disabled = true;
+      paperBtn.disabled = true;
+      scissorBtn.disabled = true;
+    }
 }
 
-//Play 5 rounds
-//   function playGame(){
-//     for(i = 1; i <= 5; i++){
-//         let playerChoice = prompt("Let's play a game! Pick rock, paper, or scissors.");
-//         const playerSelection = playerChoice.toLowerCase();
-//         playRound(playerSelection, getComputerChoice());
-//     }
-//   }
-  
-//   console.log(playGame());
+
+function resetGame() {
+  humanPlayer = 0;
+  computerPlayer = 0;
+  round = 0;
+  humanChoice.textContent = "Players Choice:";
+  computerChoice.textContent = "Computers Choice:";
+  human.textContent = "Player Score: " + 0;
+  computer.textContent = "Computer Score: " + 0;
+  div.textContent = "";
+  winner.textContent = "";
+  resetBtn.style.display = "none";
+  rockBtn.disabled = false;
+  paperBtn.disabled = false;
+  scissorBtn.disabled = false;
+}
